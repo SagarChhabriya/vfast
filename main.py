7c32395fae15324c947c6c85ccb09ff4c224ff0f
SagarChhabriya <sagarchhabriya.bscskdkf22@iba-suk.edu.pk>
2025-08-30 23:45:21 +0500

    Add in memory db

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from pathlib import Path
+import json
 
 app = FastAPI()
 
@@ -13,3 +15,13 @@ async def hello_name(name: str):
 @app.get("/health")
 async def health_check():
     return {"status": "OK", "message": "API is running!"}
+
+
+def load_data():
+    with open("patients.json", "rb") as file:
+        data = json.load(file)
+    return data
+
+@app.get("/view")
+async def view_records():
+    return load_data()
